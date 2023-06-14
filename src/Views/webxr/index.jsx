1862e1b51a09eaeeb49a4288eac2712ff0265ff2
OpEnD17 <w763023941@gmail.com>
2023-06-15 00:24:38 +0930

    test

--- src/Views/webxr/index.jsx
+++ src/Views/webxr/index.jsx
@@ -27,6 +27,22 @@ const JitsiMeetJS = window.JitsiMeetJS;
 const conf = JitsiMeetJS.events.conference;
 const conn = JitsiMeetJS.events.connection;
 
+A.registerComponent('play', {
+    init: function () {
+        var myEL = document.querySelector("#yellow");
+        this.el.addEventListener('click', function () {
+            myEL.components.sound.playSound();
+        });
+    }
+});
+A.registerComponent('stop', {
+    init: function () {
+        var myEL = document.querySelector("#yellow");
+        this.el.addEventListener('click', function () {
+            myEL.components.sound.stopSound();
+        });
+    }
+});
 
 const WebXR = () => {
 
@@ -316,7 +332,7 @@ const WebXR = () => {
 
                 {/* <!--movement--> */}
                 <Entity id='cameraRig' ref={rigRef} >
-                    <Entity ref={camRef} primitive="a-camera" user-height="1.6" send-pos wasd-controls-enabled="true" look-controls="enabled:true">
+                    <Entity ref={camRef} primitive="a-camera" user-height="1.6" send-pos wasd-controls-enabled="true" look-controls="pointerLockEnabled:true">
                         <Entity
                             primitive="a-cursor"
                             cursor={{ fuse: false }}
@@ -355,19 +371,119 @@ const WebXR = () => {
                     <image id="face9" src={face9} />
                 </a-assets>
 
+                {/* <!--radio--> */}
+                <a-entity data-brackets-id="327" id="yellow" gltf-model="aframe/radio/scene.gltf" position="9.11851 0.45804 -7.96256" sound="src: mpi,Benjamin%20-%20Inferno.mp3" scale="0.2 0.2 0.2" rotation="0 17.71 0">
+                    <a-sphere
+                        data-brackets-id="328"
+                        color="#00AA00" radius="0.2"
+                        position="-0.5 0.93319 0"
+                        play
+                        animation__scale="dur: 200; property: scale; startEvents: mouseenter; to: 1.2 1.2 1.2"
+                        animation__scale_reverse="dur: 200; property: scale; startEvents: mouseleave; to: 1 1 1">
+
+                    </a-sphere>
+                    <a-sphere
+                        data-brackets-id="329"
+                        color="#AA0000"
+                        radius="0.2"
+                        position="0.5 1 0"
+                        stop
+                        animation__scale="dur: 200; property: scale; startEvents: mouseenter; to: 1.2 1.2 1.2"
+                        animation__scale_reverse="dur: 200; property: scale; startEvents: mouseleave; to: 1 1 1"></a-sphere>
+                </a-entity>
+                <a-box color="yellow"
+                    height="0.422" width="0.39" depth="0.75"
+                    position="9. 0.26 -7.98"
+                    data-brackets-id="440"
+                    animation__scale="dur: 200; property: scale; startEvents: mouseenter; to: 1.2 1.2 1.2"
+                    animation__scale_reverse="dur: 200; property: scale; startEvents: mouseleave; to: 1 1 1"
+                    showtext>
+                </a-box>
+
+
+
+                {/* <!--fence--> */}
+                <a-entity id="fence" gltf-model="aframe/fence/scene.gltf" position="-7.01957 0.01 9.91373" scale="1 2 2" rotation="0 90 0"></a-entity>
+                <a-entity id="fence" gltf-model="aframe/fence/scene.gltf" position="-4.01957 0.01001 9.91373" scale="1 2 2" rotation="0 90 0"></a-entity>
+                <a-entity id="fence" gltf-model="aframe/fence/scene.gltf" position="-1.01957 0.01001 9.91373" scale="1 2 2" rotation="0 90 0"></a-entity>
+                <a-entity id="fence" gltf-model="aframe/fence/scene.gltf" position="2.01957 0.01001 9.91373" scale="1 2 2" rotation="0 90 0"></a-entity>
+                <a-entity id="fence" gltf-model="aframe/fence/scene.gltf" position="5.01957 0.01001 9.91373" scale="1 2 2" rotation="0 90 0"></a-entity>
+                <a-entity id="fence" gltf-model="aframe/fence/scene.gltf" position="8.01957 0.01001 9.91373" scale="1 2 2" rotation="0 90 0"></a-entity>
+                <a-entity id="fence" gltf-model="aframe/fence/scene.gltf" position="9.61447 0.01001 9.91373" scale="1 2 1" rotation="0 90 0"></a-entity>
+
                 {/* <!--ceiling--> */}
                 <a-box id='ceil' color="white" height="1" width="20" depth="20" position="0 4.5 0"></a-box>
+
                 {/* <!--wall--> */}
-                <a-box color="white" position="9.9 2 0" material="" geometry="height: 4; width: 0.2; depth: 20"></a-box>
-                <a-box color="white" position="0 2 -9.9" material="" geometry="height:4; width: 20; depth: 0.2"></a-box>
-                <a-box color="white" position="7.3 2 7.5" material="" geometry="height: 4; width: 5; depth: 0.2"></a-box>
+                <a-box class='pointable' color="white" position="9.9 2 0" material="" geometry="height: 4; width: 0.2; depth: 20"></a-box>
+                <a-box class='pointable' color="white" position="0 2 -9.9" material="" geometry="height:4; width: 20; depth: 0.2"></a-box>
+                <a-box class='pointable' color="white" position="7.3 2 7.5" material="" geometry="height: 4; width: 5; depth: 0.2"></a-box>
                 <a-entity class='pointable' gltf-model="aframe/window/scene.gltf" position="3.82 -1.0 7.7" scale="0.04 0.074 0.04" rotation="0 60 0"></a-entity>
-                <a-box color="white" position="-7.3 2 7.5" material="" geometry="height: 4; width: 5; depth: 0.2"></a-box>
-                <a-entity gltf-model="aframe/window/scene.gltf" position="-3.86296 -1.0 7.7" scale="0.04 0.073 0.04" rotation="0 60 0"></a-entity>
-                <a-box color="white" position="0.04123 2 7.5" material="" geometry="height: 4; width: 5.82; depth: 0.2"></a-box>
-                <a-box color="white" position="-3.83305 3.75848 7.5" material="" geometry="height: 0.55; width: 1.96; depth: 0.2"></a-box>
-                <a-box color="white" position="3.89924 3.75848 7.5" material="" geometry="height: 0.55; width: 1.96; depth: 0.2"></a-box>
-                <a-box color="white" position="-9.9 2 -5.01609" material="" geometry="height: 4; width: 0.2; depth: 9.94"></a-box>
+                <a-box class='pointable' color="white" position="-7.3 2 7.5" material="" geometry="height: 4; width: 5; depth: 0.2"></a-box>
+                <a-entity class='pointable' gltf-model="aframe/window/scene.gltf" position="-3.86296 -1.0 7.7" scale="0.04 0.073 0.04" rotation="0 60 0"></a-entity>
+                <a-box class='pointable' color="white" position="0.04123 2 7.5" material="" geometry="height: 4; width: 5.82; depth: 0.2"></a-box>
+                <a-box class='pointable' color="white" position="-3.83305 3.75848 7.5" material="" geometry="height: 0.55; width: 1.96; depth: 0.2"></a-box>
+                <a-box class='pointable' color="white" position="3.89924 3.75848 7.5" material="" geometry="height: 0.55; width: 1.96; depth: 0.2"></a-box>
+                <a-box class='pointable' color="white" position="-9.9 2 -5.01609" material="" geometry="height: 4; width: 0.2; depth: 9.94"></a-box>
+
+                {/* <!--Coffee cup--> */}
+                <a-entity data-brackets-id="203" class="pointable" gltf-model="aframe/coffee_shop_cup/scene.gltf" position="1.0 0.8 2.45" scale="0.25 0.25 0.25" animation__scale="dur: 200; property: scale; startEvents: mouseenter; to: 0.27 0.27 0.27"
+                    animation__scale_reverse="dur: 200; property: scale; startEvents: mouseleave; to: 0.25 0.25 0.25"></a-entity>
+                <a-entity data-brackets-id="203" class="pointable" gltf-model="aframe/coffee_shop_cup/scene.gltf" position="-0.5 0.8 2.45" scale="0.25 0.25 0.25" animation__scale="dur: 200; property: scale; startEvents: mouseenter; to: 0.27 0.27 0.27"
+                    animation__scale_reverse="dur: 200; property: scale; startEvents: mouseleave; to: 0.25 0.25 0.25"></a-entity>
+                <a-entity data-brackets-id="203" class="pointable" gltf-model="aframe/coffee_shop_cup/scene.gltf" position="-2.2 0.8 2.45" scale="0.25 0.25 0.25" animation__scale="dur: 200; property: scale; startEvents: mouseenter; to: 0.27 0.27 0.27"
+                    animation__scale_reverse="dur: 200; property: scale; startEvents: mouseleave; to: 0.25 0.25 0.25"></a-entity>
+                <a-entity data-brackets-id="203" class="pointable" gltf-model="aframe/coffee_shop_cup/scene.gltf" position="1.0 0.8 0.55" scale="0.25 0.25 0.25" animation__scale="dur: 200; property: scale; startEvents: mouseenter; to: 0.27 0.27 0.27"
+                    animation__scale_reverse="dur: 200; property: scale; startEvents: mouseleave; to: 0.25 0.25 0.25"></a-entity>
+                <a-entity data-brackets-id="203" class="pointable" gltf-model="aframe/coffee_shop_cup/scene.gltf" position="-0.5 0.8 0.55" scale="0.25 0.25 0.25" animation__scale="dur: 200; property: scale; startEvents: mouseenter; to: 0.27 0.27 0.27"
+                    animation__scale_reverse="dur: 200; property: scale; startEvents: mouseleave; to: 0.25 0.25 0.25"></a-entity>
+                <a-entity data-brackets-id="203" class="pointable" gltf-model="aframe/coffee_shop_cup/scene.gltf" position="-2.2 0.8 0.55" scale="0.25 0.25 0.25" animation__scale="dur: 200; property: scale; startEvents: mouseenter; to: 0.27 0.27 0.27"
+                    animation__scale_reverse="dur: 200; property: scale; startEvents: mouseleave; to: 0.25 0.25 0.25"></a-entity>
+
+                {/* <!--UI--> */}
+                <a-entity id="UI1" position="9.0 2.5 -3" rotation="0 -90 0" visible="false" screenlocation>
+                    <a-plane width="2" height="1" color="blue" opacity="0.8">
+                        <a-entity text="value: Hello World; align: center; " scale="7 7 7"></a-entity>
+                    </a-plane>
+                </a-entity>
+
+                <a-entity id="introText" visible="false" position="9 1.26 -7.98" rotation="0 -60 0" >
+                    <a-plane width="2" height="1" color="green" opacity="0.8" side="double">
+                        <a-entity text="value: Music !!; align: center; " scale="7 7 7"></a-entity>
+                    </a-plane>
+                </a-entity>
+
+
+                {/* <!--VENDING machine--> */}
+                <a-entity data-brackets-id="70" gltf-model="aframe/vending_machine_coca_cola/scene.gltf" position="12.81719 1.162 -4.26055" rotation="0 -90 0" scale="1 1 2"></a-entity>
+                <a-entity data-brackets-id="70" gltf-model="aframe/vending_machine_coca_cola/scene.gltf" position="12.81719 1.162 -2.95598" rotation="0 -90 0" scale="1 1 2"></a-entity>
+
+                {/* <!--chandelier--> */}
+                <a-entity id="chandelier" gltf-model="aframe/chandelier/scene.gltf" position="-0.57597 3.95402 -1.03613" rotation="0 -90 0" scale="0.003 0.002 0.005"></a-entity>
+
+                {/* <!--plants_foliage_study--> */}
+                <a-entity id="plant" gltf-model="aframe/plants_foliage_study/scene.gltf" position="8.10378 0.45055 8.88699" rotation="" scale="1 1 0.9"></a-entity>
+                <a-entity id="plant" gltf-model="aframe/plants_foliage_study/scene.gltf" position="4.10378 0.45055 8.88699" rotation="" scale="1 1 0.9"></a-entity>
+                <a-entity id="plant" gltf-model="aframe/plants_foliage_study/scene.gltf" position="0.10378 0.45055 8.88699" rotation="" scale="1 1 0.9"></a-entity>
+                <a-entity id="plant" gltf-model="aframe/plants_foliage_study/scene.gltf" position="-4.10378 0.45055 8.88699" rotation="" scale="1 1 0.9"></a-entity>
+
+                {/* <!--photoframe--> */}
+                <a-entity data-brackets-id="70" gltf-model="aframe/3d_architecture__photo_frame/scene.gltf" position="0.52 3.041 -9.8" rotation="0 -90 0" scale="1 0.8 1.7"></a-entity>
+                <a-entity data-brackets-id="67" gltf-model="aframe/3d_architecture__photo_frame/scene.gltf" position="-0.464 2.3 -9.8" rotation="0 -90 0" scale="1 0.9 1.5"></a-entity>
+                <a-entity data-brackets-id="32" gltf-model="aframe/3d_architecture__photo_frame/scene.gltf" position="0.51 1.85 -9.8" rotation="0 -90 0" scale="1 2 1.7"></a-entity>
+                <a-entity data-brackets-id="68" gltf-model="aframe/3d_architecture__photo_frame/scene.gltf" position="-0.31 1.46 -9.8" rotation="0 -90 0"></a-entity>
+                <a-entity data-brackets-id="71" gltf-model="aframe/3d_architecture__photo_frame/scene.gltf" position="-0.95 1.45 -9.8" rotation="0 -90 0"></a-entity>
+                <a-entity data-brackets-id="69" gltf-model="aframe/3d_architecture__photo_frame/scene.gltf" position="1.35 2.24 -9.8" rotation="0 -90 0" scale=""></a-entity>
+                <a-entity data-brackets-id="72" gltf-model="aframe/3d_architecture__photo_frame/scene.gltf" position="1.44 1.42 -9.8" rotation="0 -90 0" scale="1 0.9 1.3"></a-entity>
+
+                {/*photes*/}
+                <a-image src="aframe/O.jpg" position="0.51 1.85 -9.78" scale="0.91 1.47 1"></a-image>
+                <a-image src="aframe/Q.jpg" position="-0.464 2.3 -9.78" scale="0.76 0.66 1"></a-image>
+                <a-image src="aframe/tooopen.jpg" position="0.52 3.041 -9.78" scale="0.84 0.58 1"></a-image>
+                <a-image src="aframe/R.jpg" position="-0.95 1.45 -9.78" scale="0.51 0.72 1"></a-image>
+                <a-image src="aframe/C.jpg" position="-0.31 1.46 -9.78" scale="0.5 0.75 1"></a-image>
+                <a-image src="aframe/a.jpg" position="1.35 2.24 -9.78" scale="0.49 0.74 1"></a-image>
+                <a-image src="aframe/b.jpg" position="1.44 1.42 -9.78" scale="0.64 0.66 1"></a-image>
             </Scene>
         </div>
     );
